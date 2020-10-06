29bded1daed0bb274b1cec219bc11c922e33c8d3
conritai <vuphamthanhnguyen@gmail.com>
2020-10-06 11:30:56 +0700

    Add classes

--- eShopSolution.Data/EF/EShopDBContext.cs
+++ eShopSolution.Data/EF/EShopDBContext.cs
@@ -1,8 +1,17 @@
-﻿using System;
+﻿using eShopSolution.Data.EF.Entity;
+using Microsoft.EntityFrameworkCore;
+using System;
 
 namespace eShopSolution.Data
 {
-    public class EShopDBContext
+    public class EShopDBContext : DbContext
     {
+        public EShopDBContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<Product> Products { set; get; }
+        public DbSet<Category> Categories { set; get; }
+
     }
 }
