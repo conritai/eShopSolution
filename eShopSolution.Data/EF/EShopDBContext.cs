c3ad72e469d290a1f0985c90e567a7c1e99270bd
conritai <vuphamthanhnguyen@gmail.com>
2020-10-06 11:03:07 +0700

    add package

---
+++ eShopSolution.Data/EF/EShopDBContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+
+namespace eShopSolution.Data
+{
+    public class EShopDBContext : DbContext
+    {
+        public EShopDBContext(DbContextOptions options) : base(options)
+        {
+            options.UseSqlServer
+        }
+
+        prot
+    }
+}
